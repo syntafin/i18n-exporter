5ee35f0747485011e354aebe6b8b65a65aed6aba
Syntafin <hello@syntafin.dev>
2025-04-22 19:56:32 +0200

    Refactor i18n-exporter config for clarity and extensibility
    Updated the i18n-exporter configuration to include detailed comments, improve readability, and support additional options like framework type and sorting method. These changes enhance flexibility and ensure better maintainability of the configuration file.

--- config/i18n-exporter.php
+++ config/i18n-exporter.php
@@ -1,12 +1,56 @@
 <?php
 
 return [
-    'path' => 'resources/lang',
+    /**
+     * Path to the language files from Laravel.
+     *
+     * @var string
+     */
+    'path' => resource_path('lang'),
+
+    /**
+     * List of locales that should be exported.
+     *
+     * @var array
+     */
     'locales' => ['en', 'de'],
+
+    /**
+     * Set the type of framework you want to use the files with.
+     *
+     * options: vue, react, svelte
+     */
+    'type' => 'vue',
+
+    /**
+     * List of files that should be excluded from export.
+     *
+     * @var array
+     */
     'exclude' => [],
-    'include' => [],
+
+    /**
+     * Force overwrite of existing files.
+     *
+     * default: true
+     * @var bool
+     */
     'force' => env('I18N_FORCE_OVERWRITE', true),
-    'sort_by_key' => env('I18N_SORT_BY_KEY', false),
-    'sort_by_value' => env('I18N_SORT_BY_VALUE',false),
+
+    /**
+     * Sort the exported files by key or value.
+     *
+     * default: key
+     * options: key, value
+     * @var string
+     */
+    'sort_by' => env('I18N_SORT_BY', 'key'),
+
+    /**
+     * Run the export command on optimize.
+     *
+     * default: false
+     * @var bool
+     */
     'run_on_optimize' => env('I18N_RUN_ON_OPTIMIZE', false),
 ];
